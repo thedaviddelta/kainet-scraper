db33f1080de9c22847713eabd32b420a7c676e18
David <info@thedaviddelta.com>
2021-04-15 22:23:54 +0200

    Added video, album, playlist & artist search

--- src/search.ts
+++ src/search.ts
@@ -14,33 +14,117 @@ type SearchData<T extends ResultData> = {
     }
 };
 
-type ResultData = SongData;
+type ResultData = SongData | VideoData | AlbumData | PlaylistData;
 
 type SongData = {
     musicResponsiveListItemRenderer?: {
-        flexColumns?: {
-            musicResponsiveListItemFlexColumnRenderer?: {
-                text?: {
-                    runs?: {
-                        text?: string,
-                        navigationEndpoint?: {
-                            watchEndpoint?: {
-                                videoId?: string
+        flexColumns?: FlexColumns,
+        thumbnail?: Thumbnail,
+        playlistItemData?: PlaylistItemData,
+    }
+};
+
+type VideoData = {
+    musicResponsiveListItemRenderer?: {
+        flexColumns?: FlexColumns,
+        thumbnail?: Thumbnail,
+        playlistItemData?: PlaylistItemData,
+    }
+};
+
+type AlbumData = {
+    musicResponsiveListItemRenderer?: {
+        flexColumns?: FlexColumns,
+        thumbnail?: Thumbnail,
+        menu?: Menu,
+        overlay?: Overlay
+    }
+};
+
+type PlaylistData = {
+    musicResponsiveListItemRenderer?: {
+        flexColumns?: FlexColumns,
+        thumbnail?: Thumbnail,
+        menu?: Menu,
+        overlay?: Overlay
+    }
+};
+
+type ArtistData = {
+    musicResponsiveListItemRenderer?: {
+        flexColumns?: FlexColumns,
+        thumbnail?: Thumbnail,
+        navigationEndpoint?: NavigationEndpoint
+    }
+};
+
+type FlexColumns = {
+    musicResponsiveListItemFlexColumnRenderer?: {
+        text?: {
+            runs?: {
+                text?: string,
+                navigationEndpoint?: {
+                    watchEndpoint?: {
+                        videoId?: string
+                    }
+                },
+                browseEndpoint?: {
+                    browseId?: string
+                }
+            }[]
+        }
+    }
+}[];
+
+type Thumbnail = {
+    musicThumbnailRenderer?: {
+        thumbnail?: {
+            thumbnails?: {
+                url?: string
+            }[]
+        }
+    }
+};
+
+type PlaylistItemData = {
+    videoId?: string
+};
+
+type Menu = {
+    menuRenderer?: {
+        items?: {
+            menuNavigationItemRenderer: {
+                toggleMenuServiceItemRenderer?: {
+                    toggledServiceEndpoint?: {
+                        likeEndpoint?: {
+                            target?: {
+                                playlistId?: string
                             }
                         }
-                    }[]
+                    }
                 }
             }
-        }[],
-        thumbnail?: {
-            musicThumbnailRenderer?: {
-                thumbnail?: {
-                    thumbnails?: {
-                        url?: string
-                    }[]
+        }[]
+    }
+};
+
+type Overlay = {
+    musicItemThumbnailOverlayRenderer?: {
+        content?: {
+            musicPlayButtonRenderer?: {
+                playNavigationEndpoint?: {
+                    watchPlaylistEndpoint?: {
+                        playlistId?: string
+                    }
                 }
             }
         }
+    }
+};
+
+type NavigationEndpoint = {
+    browseEndpoint?: {
+        browseId?: string
     }
 };
 
@@ -55,15 +139,52 @@ interface Song {
     album?: string,
     duration?: number,
     durationText?: string,
-    thumbnail?: string
+    thumbnail?: (string|undefined)[]
 }
 
-export const getSongs = (query: string, lang?: string): Promise<Song[] | null> => {
-    const extractText = (data: SongData, colIndex: number, runIndex: number) => (
-        data.musicResponsiveListItemRenderer?.flexColumns?.[colIndex]?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.[runIndex]?.text
-    );
+interface Video {
+    id?: string,
+    title?: string,
+    artist?: string,
+    duration?: number,
+    durationText?: string,
+    thumbnail?: (string|undefined)[],
+    views?: bigint
+}
 
-    const durationToSeconds = (text?: string) => {
+interface Album {
+    id?: string,
+    title?: string,
+    artist?: string,
+    thumbnail?: (string|undefined)[],
+    year?: string
+}
+
+interface Playlist {
+    id?: string,
+    title?: string,
+    thumbnail?: (string|undefined)[],
+    songCount?: number
+}
+
+interface Artist {
+    id?: string,
+    name?: string,
+    thumbnail?: (string|undefined)[],
+    subCount?: bigint
+}
+
+const extract = {
+    text: (data: ResultData, colIndex: number, runIndex: number) => {
+        const cols = data.musicResponsiveListItemRenderer?.flexColumns;
+        const currCol = cols?.[colIndex >= 0 ? colIndex : cols.length + colIndex];
+        const runs = currCol?.musicResponsiveListItemFlexColumnRenderer?.text?.runs;
+        return runs?.[runIndex >= 0 ? runIndex : runs.length + runIndex]?.text;
+    },
+    thumbnails: (data: ResultData) => (
+        data.musicResponsiveListItemRenderer?.thumbnail?.musicThumbnailRenderer?.thumbnail?.thumbnails?.map(t => t.url)
+    ),
+    duration: (text?: string) => {
         const parts = text?.split(":");
         if (!parts || parts.length < 2 || parts.length > 3)
             return undefined;
@@ -73,27 +194,155 @@ export const getSongs = (query: string, lang?: string): Promise<Song[] | null> =
         }
         const [mins, secs] = parts;
         return +mins * 60 + +secs;
-    };
+    },
+    num: {
+        simple: (text?: string) => {
+            const num = text?.replace(/\D+/, "");
+            if (!num || Number.isNaN(+num))
+                return undefined;
+            return +num;
+        },
+        big: (text?: string) => {
+            const multipliers = {
+                "K": 1E3,
+                "M": 1E6,
+                "B": 1E9
+            } as {
+                [key: string]: number
+            };
 
-    return client(
+            const [, count, , multiplier] = text?.match(/([\d]+([.,][\d]+)?)\s?([KMBkmb])?/) ?? [];
+            if (!count || Number.isNaN(+count))
+                return undefined;
+            return BigInt(Math.round(+count * 100)) * BigInt(multipliers[multiplier] ?? 1) / 100n;
+        }
+    },
+    id: {
+        songOrVideo: (data: SongData | VideoData) => (
+            data.musicResponsiveListItemRenderer?.playlistItemData?.videoId
+            ?? data.musicResponsiveListItemRenderer?.flexColumns?.[0]?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.[0]?.navigationEndpoint?.watchEndpoint?.videoId
+        ),
+        albumOrPlaylist: (data: AlbumData | PlaylistData) => (
+            data.musicResponsiveListItemRenderer?.menu?.menuRenderer?.items?.[4]?.menuNavigationItemRenderer?.toggleMenuServiceItemRenderer?.toggledServiceEndpoint?.likeEndpoint?.target?.playlistId
+            ?? data.musicResponsiveListItemRenderer?.overlay?.musicItemThumbnailOverlayRenderer?.content?.musicPlayButtonRenderer?.playNavigationEndpoint?.watchPlaylistEndpoint?.playlistId
+        ),
+        artist: (data: ArtistData) => (
+            data.musicResponsiveListItemRenderer?.navigationEndpoint?.browseEndpoint?.browseId
+        )
+    }
+};
+
+const parse = {
+    songs: (data: SongData): Song => ({
+        id: extract.id.songOrVideo(data),
+        title: extract.text(data, 0, 0),
+        artist: extract.text(data, 1, 0),
+        album: extract.text(data, 1, -3),
+        duration: extract.duration(extract.text(data, 1, -1)),
+        durationText: extract.text(data, 1, -1),
+        thumbnail: extract.thumbnails(data),
+    }),
+    videos: (data: VideoData): Video => ({
+        id: extract.id.songOrVideo(data),
+        title: extract.text(data, 0, 0),
+        artist: extract.text(data, 1, 0),
+        duration: extract.duration(extract.text(data, 1, -1)),
+        durationText: extract.text(data, 1, -1),
+        thumbnail: extract.thumbnails(data),
+        views: extract.num.big(extract.text(data, 1, -3))
+    }),
+    albums: (data: AlbumData): Album => ({
+        id: extract.id.albumOrPlaylist(data),
+        title: extract.text(data, 0, 0),
+        artist: extract.text(data, 1, 2),
+        thumbnail: extract.thumbnails(data),
+        year: extract.text(data, 1, 4)
+    }),
+    playlist: (data: PlaylistData): Playlist => ({
+        id: extract.id.albumOrPlaylist(data),
+        title: extract.text(data, 0, 0),
+        thumbnail: extract.thumbnails(data),
+        songCount: extract.num.simple(extract.text(data, 1, 2))
+    }),
+    artist: (data: ArtistData): Artist => ({
+        id: extract.id.artist(data),
+        name: extract.text(data, 0, 0),
+        thumbnail: extract.thumbnails(data),
+        subCount: extract.num.big(extract.text(data, 1, 2))
+    })
+};
+
+export const getSongs = (query: string, lang?: string): Promise<Song[] | null> => (
+    client(
         url,
         {
             params: "Eg-KAQwIARAAGAAgACgAMABqChAEEAUQAxAKEAk%3D",
             query
         },
         lang
-    ).then(res => {
-        const songs = parseSearch<SongData>(res.data);
-        return songs?.map(data => ({
-            title: extractText(data, 0, 0),
-            artist: extractText(data, 1, 0),
-            album: extractText(data, 1, 2),
-            durationText: extractText(data, 1, 4),
-            duration: durationToSeconds(extractText(data, 1, 4)),
-            id: data.musicResponsiveListItemRenderer?.flexColumns?.[0]?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.[0]?.navigationEndpoint?.watchEndpoint?.videoId,
-            thumbnail: data.musicResponsiveListItemRenderer?.thumbnail?.musicThumbnailRenderer?.thumbnail?.thumbnails?.[0]?.url
-        })) ?? null;
-    }).catch(
+    ).then(res =>
+        parseSearch<SongData>(res.data)?.map(parse.songs) ?? null
+    ).catch(
         () => null
-    );
-};
+    )
+);
+
+export const getVideos = (query: string, lang?: string): Promise<Video[] | null> => (
+    client(
+        url,
+        {
+            params: "Eg-KAQwIABABGAAgACgAMABqChAEEAUQAxAKEAk%3D",
+            query
+        },
+        lang
+    ).then(res =>
+        parseSearch<VideoData>(res.data)?.map(parse.videos) ?? null
+    ).catch(
+        () => null
+    )
+);
+
+export const getAlbums = (query: string, lang?: string): Promise<Album[] | null> => (
+    client(
+        url,
+        {
+            params: "Eg-KAQwIABAAGAEgACgAMABqChAEEAUQAxAKEAk%3D",
+            query
+        },
+        lang
+    ).then(res =>
+        parseSearch<AlbumData>(res.data)?.map(parse.albums) ?? null
+    ).catch(
+        () => null
+    )
+);
+
+export const getPlaylists = (query: string, lang?: string): Promise<Playlist[] | null> => (
+    client(
+        url,
+        {
+            params: "Eg-KAQwIABAAGAAgACgBMABqChAEEAUQAxAKEAk%3D",
+            query
+        },
+        lang
+    ).then(res =>
+        parseSearch<PlaylistData>(res.data)?.map(parse.playlist) ?? null
+    ).catch(
+        () => null
+    )
+);
+
+export const getArtists = (query: string, lang?: string): Promise<Artist[] | null> => (
+    client(
+        url,
+        {
+            params: "Eg-KAQwIABAAGAAgASgAMABqChAEEAUQAxAKEAk%3D",
+            query
+        },
+        lang
+    ).then(res =>
+        parseSearch<ArtistData>(res.data)?.map(parse.artist) ?? null
+    ).catch(
+        () => null
+    )
+);
--- tests/search.test.ts
+++ tests/search.test.ts
@@ -1,33 +1,106 @@
 import faker from "faker";
-import { getSongs } from "../src/search";
+import { getSongs, getVideos, getAlbums, getPlaylists, getArtists } from "../src/search";
+
+const queries = [
+    "queen",
+    "the wall",
+    "despacito"
+];
+
+const resFromQueries = <T>(callback: (query: string) => Promise<T>): Promise<T[]> => Promise.all(queries.map(query => callback(query)));
 
 describe("songs", () => {
-    const queries = [
-        "gangnam style",
-        "el diluvi",
-        "a night at the opera"
-    ];
-
-    const resFromQueries = <T>(callback: (query: string) => Promise<T>): Promise<T[]> => Promise.all(queries.map(query => callback(query)));
-
-    it("returns youtube response correctly", async () => {
+    it("returns youtube songs with all data correctly", async () => {
         const results = await resFromQueries(getSongs);
-        results.forEach(songList => expect(songList).not.toBeNull());
-    });
-
-    it("returns all data correctly", async () => {
-        const results = await resFromQueries(getSongs);
-        results.forEach(songList => (
+        results.forEach(songList => {
+            expect(songList).not.toBeNull();
             songList?.forEach(song => (
                 Object.values(song).forEach(val => (
                     expect(val).toBeTruthy()
                 ))
-            ))
-        ));
+            ));
+        });
     });
 
-    it("returns null on no results", async () => {
+    it("returns null on no songs results", async () => {
         const songs = await getSongs(faker.git.commitSha());
         expect(songs).toBeNull();
+    });
+});
+
+describe("videos", () => {
+    it("returns youtube videos with all data correctly", async () => {
+        const results = await resFromQueries(getVideos);
+        results.forEach(videoList => {
+            expect(videoList).not.toBeNull();
+            videoList?.forEach(video => (
+                Object.values(video).forEach(val => (
+                    expect(val).toBeTruthy()
+                ))
+            ));
+        });
+    });
+
+    it("returns null on no videos results", async () => {
+        const videos = await getVideos(faker.git.commitSha());
+        expect(videos).toBeNull();
+    });
+});
+
+describe("albums", () => {
+    it("returns youtube albums with all data correctly", async () => {
+        const results = await resFromQueries(getAlbums);
+        results.forEach(albumList => {
+            expect(albumList).not.toBeNull();
+            albumList?.forEach(album => (
+                Object.values(album).forEach(val => (
+                    expect(val).toBeTruthy()
+                ))
+            ));
+        });
+    });
+
+    it("returns null on no albums results", async () => {
+        const albums = await getAlbums(faker.git.commitSha());
+        expect(albums).toBeNull();
+    });
+});
+
+describe("playlists", () => {
+    it("returns youtube playlists with all data correctly", async () => {
+        const results = await resFromQueries(getPlaylists);
+        results.forEach(playlistList => {
+            expect(playlistList).not.toBeNull();
+            playlistList?.forEach(playlist => (
+                Object.values(playlist).forEach(val => (
+                    expect(val).toBeTruthy()
+                ))
+            ));
+        });
+    });
+
+    it("returns null on no playlists results", async () => {
+        const playlists = await getPlaylists(faker.git.commitSha());
+        expect(playlists).toBeNull();
+    });
+});
+
+describe("artists", () => {
+    it("returns youtube artists with all data correctly", async () => {
+        const results = await resFromQueries(getArtists);
+        results.forEach(artistList => {
+            console.log(artistList)
+            expect(artistList).not.toBeNull();
+            artistList?.forEach(artist => (
+                Object.values(artist).forEach(val => (
+                    expect(val).toBeTruthy()
+                ))
+            ));
+        });
+    });
+
+    it("returns null on no artists results", async () => {
+        const artists = await getArtists(faker.git.commitSha());
+        expect(artists).toBeNull();
     });
 });
